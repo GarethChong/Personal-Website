813d7e0622e65b312bf3aa2fb043856c9560cf38
Gareth <garethkh.chong@gmail.com>
2026-07-21 18:07:13 +0800

    fixed the card image

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,14 @@
 import { useRef } from 'react'
 import getClickOrigin from '../utils/getClickOrigin.js'
-import cardsImg from '../assets/cards_new.png'
+import cardsImg from '../assets/cards.png'
 
 function Cards({ onClick }) {
     const myRef = useRef(null)
 
     return (
         <div ref={myRef}
-            className="absolute w-[24vw] top-[55%] right-[29%] cursor-pointer group"
+            className="absolute w-[17vw] top-[57%] right-[33%] cursor-pointer group"
+            style={{ transform: 'rotate(20deg)' }}
             onClick={() => onClick(getClickOrigin("cards", myRef, onClick))}
         >
             <img src={cardsImg} className="w-full desk-object shadow-strong" />
